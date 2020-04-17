689415b33c629f91f0bce8055bafef3b2d6bbce4
krivoruchkoND <lizey@bk.ru>
2020-04-17 11:40:44 +0300

    Updates

---
+++ precache-manifest.b5c72f5e2f2fb69c7c5f568c7c40eff5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17493cb5fcdbdaa4274efd45cf4b1157",
+    "url": "/krivoruchkoND/lunch-menu.git/index.html"
+  },
+  {
+    "revision": "f3179cf9176446e0b98d",
+    "url": "/krivoruchkoND/lunch-menu.git/static/css/2.53f2de8a.chunk.css"
+  },
+  {
+    "revision": "dac00835367e6c0f119b",
+    "url": "/krivoruchkoND/lunch-menu.git/static/css/main.ed6ac0a0.chunk.css"
+  },
+  {
+    "revision": "f3179cf9176446e0b98d",
+    "url": "/krivoruchkoND/lunch-menu.git/static/js/2.2eb75b0b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/krivoruchkoND/lunch-menu.git/static/js/2.2eb75b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac00835367e6c0f119b",
+    "url": "/krivoruchkoND/lunch-menu.git/static/js/main.60d970c3.chunk.js"
+  },
+  {
+    "revision": "24aff47fb3bc742ef177",
+    "url": "/krivoruchkoND/lunch-menu.git/static/js/runtime-main.0ac7233c.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/krivoruchkoND/lunch-menu.git/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/krivoruchkoND/lunch-menu.git/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/krivoruchkoND/lunch-menu.git/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/krivoruchkoND/lunch-menu.git/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/krivoruchkoND/lunch-menu.git/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
