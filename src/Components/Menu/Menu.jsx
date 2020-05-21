8f1689b86450e69f60e3583a449d58b778431e10
krivoruchkoND <lizey@bk.ru>
2020-05-21 13:56:33 +0300

    menu component done

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import saladIcon from '../../Icons/salad.png';
+import soupIcon from '../../Icons/soup.png';
+import dishesIcon from '../../Icons/dishes.png';
+import dessertIcon from '../../Icons/dessert.png';
+export default function Menu() {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+          <ul className="navbar-nav">
+              <li className="nav-item">
+                  <a className="nav-link" href='#salads'>Salads</a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link" href='#soups'>Soups</a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link" href='#mains'>Mains</a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link" href='#dessert'>Dessert</a>
+              </li>
+          </ul>
+      </nav>
+      <section id='salads'>
+          <div className="card text-body text-decoration-none mb-3">
+              <h2 className="card-header">
+                  Salads
+              </h2>
+              <ul className="list-group list-group-flush">
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={saladIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Salad 1</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={saladIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Salad 2</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={saladIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Salad 3</h3></li>
+              </ul>
+          </div>
+      </section>
+      <section id='soups'>
+          <div className="card text-body text-decoration-none mb-3">
+              <h2 className="card-header">
+                  Soups
+              </h2>
+              <ul className="list-group list-group-flush">
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={soupIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Soup 1</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={soupIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Soup 2</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={soupIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Soup 3</h3></li>
+              </ul>
+          </div>
+      </section>
+      <section id='mains'>
+          <div className="card text-body text-decoration-none mb-3">
+              <h2 className="card-header">
+                  Mains
+              </h2>
+              <ul className="list-group list-group-flush">
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={dishesIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Main 1</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={dishesIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Main 2</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={dishesIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Main 3</h3></li>
+              </ul>
+          </div>
+      </section>
+      <section id='dessert'>
+          <div className="card text-body text-decoration-none mb-3">
+              <h2 className="card-header">
+                  Desserts
+              </h2>
+              <ul className="list-group list-group-flush">
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={dessertIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Dessert 1</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={dessertIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Dessert 2</h3></li>
+                  <li className="list-group-item d-flex align-items-center justify-content-center"><img src={dessertIcon} alt="..." className="img-fluid w-25 mr-4"/><h3>Dessert 3</h3></li>
+              </ul>
+          </div>
+      </section>
+    </div>
+  );
+}
