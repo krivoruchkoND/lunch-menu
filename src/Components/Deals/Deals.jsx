0395043680debc62dffc39a8f5ebcd6d546337f9
krivoruchkoND <lizey@bk.ru>
2020-05-22 10:14:08 +0300

    db request done

--- src/Components/Deals/Deals.jsx
+++ src/Components/Deals/Deals.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BuildInCarousel from '../BuildInCarousel';
 
 import priceIcon from '../../Icons/price.png';
-import saladIcon from '../../Icons/salad.png';
-import soupIcon from '../../Icons/soup.png';
-import dishesIcon from '../../Icons/dishes.png';
-import drinksIcon from '../../Icons/drinks.png';
 
 export default function Deals() {
+  const [lunch, setLunch] = useState([]);
+  const [pos1, setPos1] = useState({});
+  const [pos2, setPos2] = useState({});
+  const [pos3, setPos3] = useState({});
+
+  
+  async function fetchData(type) {
+    console.log('fetchData');
+    const response = await fetch("http://localhost:5000/api/day_lunch", {
+      type: 'GET',
+    });
+    response
+      .json()
+      .then(({lunch_set}) => {
+        setPos1(lunch_set.position_1);
+        setPos2(lunch_set.position_2);
+        setPos3(lunch_set.position_3)
+      })
+  };
+
+  useEffect(() => {
+      fetchData();
+  }, []);
+
   return (
     <div className="container w-75 p-3">
       <div className="card text-body mb-3">
@@ -18,23 +38,40 @@ export default function Deals() {
       </div>
       <div className="card text-body text-decoration-none mb-3">
           <ul className="list-group list-group-flush text-center">
-              <li className="list-group-item d-flex flex-column align-items-center justify-content-center"><img src={saladIcon} alt="..." className="img-fluid w-25"/><h3>Salad Name</h3></li>
-              <li className="list-group-item d-flex flex-column align-items-center justify-content-center"><img src={soupIcon} alt="..." className="img-fluid w-25"/><h3>Soup Name</h3></li>
-              <li className="list-group-item d-flex flex-column align-items-center justify-content-center"><img src={dishesIcon} alt="..." className="img-fluid w-25"/><h3>Main Course Name</h3></li>
-              <li className="list-group-item d-flex flex-column align-items-center justify-content-center"><img src={drinksIcon} alt="..." className="img-fluid w-25"/><h3>Drink Name</h3></li>
+              <li className="list-group-item d-flex flex-column align-items-center justify-content-center">
+                <img src={"http://localhost:5000" + pos1.image} alt="..." className="img-fluid w-25"/>
+                <div>
+                  <h3>{pos1.name}</h3>
+                  <p>Price: {pos1.price}</p>
+                </div>
+              </li>
+              <li className="list-group-item d-flex flex-column align-items-center justify-content-center">
+                <img src={"http://localhost:5000" + pos2.image} alt="..." className="img-fluid w-25"/>
+                <div>
+                  <h3>{pos2.name}</h3>
+                  <p>Price: {pos2.price}</p>
+                </div>
+              </li>
+              <li className="list-group-item d-flex flex-column align-items-center justify-content-center">
+                <img src={"http://localhost:5000" + pos3.image} alt="..." className="img-fluid w-25"/>
+                <div>
+                  <h3>{pos3.name}</h3>
+                  <p>Price: {pos3.price}</p>
+                </div>
+              </li>
           </ul>
       </div>
       <div className="text-center mb-3">
           <button type="button" className="btn btn-light btn-lg border"><h3 className="mt-1">Order</h3></button>
       </div>
       <BuildInCarousel imgArray={[
-        "https://via.placeholder.com/512x256?text=Mon",
-        "https://via.placeholder.com/512x256?text=Tue",
-        "https://via.placeholder.com/512x256?text=Wed",
-        "https://via.placeholder.com/512x256?text=Thu",
-        "https://via.placeholder.com/512x256?text=Fri",
-        "https://via.placeholder.com/512x256?text=Sat",
-        "https://via.placeholder.com/512x256?text=Sun"
+        "https://via.placeholder.com/256x128?text=Mon",
+        "https://via.placeholder.com/256x128?text=Tue",
+        "https://via.placeholder.com/256x128?text=Wed",
+        "https://via.placeholder.com/256x128?text=Thu",
+        "https://via.placeholder.com/256x128?text=Fri",
+        "https://via.placeholder.com/256x128?text=Sat",
+        "https://via.placeholder.com/256x128?text=Sun"
       ]} />
     </div>
   );
